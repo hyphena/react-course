0a1bbcecc43863f25efbd280ed0f12f3d6ca408e
Luna Ramirez <serenelunation@gmail.com>
2023-11-09 12:50:45 -0500

    created and styled booklist components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
-}
+import './index.css';
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 };
 
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <img src="./images/book-1.jpg" alt="The Woman in Me" />;
+const Title = () => <h2>The Woman in Me</h2>;
+const Author = () => {
+  return <h4>Britney Spears</h4>;
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
