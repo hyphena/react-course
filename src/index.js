2dd335674a92a8d9820fda1bf60266cb3010792d
Luna Ramirez <serenelunation@gmail.com>
2023-11-10 01:09:37 -0500

    set up book component props - refactored to allow dynamic presentation - added second book, a beloved classic

--- src/index.js
+++ src/index.js
@@ -3,32 +3,46 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+const firstBook = {
+  author: 'Britney Spears',
+  title: 'The Woman in Me',
+  img: './images/book-1.jpg',
+};
+
+const secondBook = {
+  author: 'Mo Willems',
+  title: "Don't let the Pigeon Drive the Sleigh!",
+  img: './images/book-2.jpg',
+};
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
 
-const Image = () => <img src="./images/book-1.jpg" alt="The Woman in Me" />;
-const Title = () => <h2>The Woman in Me</h2>;
-const Author = () => {
-  return <h4>Britney Spears</h4>;
-};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<BookList />);
